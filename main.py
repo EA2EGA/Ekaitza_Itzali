dacce37ecdbebde851afc08c10ecea31b579efd2
Xabier <xabiergarmendia@ingran.es>
2017-01-10 22:26:14 +0100

    Readme updated

--- main.py
+++ main.py
@@ -1,4 +1,65 @@
+# BSD 2-Clause License
 
+# Copyright (c) 2017, xabiergarmendia@gmail.com
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# * Redistributions of source code must retain the above copyright notice, this
+  # list of conditions and the following disclaimer.
+
+# * Redistributions in binary form must reproduce the above copyright notice,
+  # this list of conditions and the following disclaimer in the documentation
+  # and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Ekaitza Itzali
+# by EA2EGA
+#
+# Must be used with a CP2102 USB to TTL converter.
+# The CP2102 must be set up for 360 bauds instead of 300
+# and 10400 bauds instead of 14400.
+# This can be done with the AN205SW tool from Silicon Labs,
+# the manufacturer of the CP2102
+#
+# Schematic:
+#
+#         Car Obd Port       |       CP2102 USB to TTL converter
+#                            |
+# K-line      12 Volt   GND  |  GND   5 Volt    RX        TX
+#  |           |         |       |     |         |        |
+#  |           |         |-------|     |         |        |
+#  |           |                       |         |        |
+#  |--2K2------|       Reduce signal   |         |        |
+#  |                         to 0-5V   |         |        |
+#  |--22K--------1N4184->--------------|         |        |
+#  |        |                                    |        |
+#  |        |----------2K2-----------------------|        |
+#  |                                                      |
+#  |                                                      |
+#  |     2N2222A              (Invert again and Power)    |
+#  |     C  B  E                                          |
+#  |     |  |  |-------GND                                |
+#  |------  |                                             |
+#           |                                             |
+#           |--------|                                    |
+#                    |                                    |
+#       5 Volt       |  2N2222A      (Inverter)           |
+#          |-----2K2----C  B  E                           |
+#                          |  |----- GND                  |
+#                          |                              |
+#                          |------------------2K2---------|
 
 
 import time
