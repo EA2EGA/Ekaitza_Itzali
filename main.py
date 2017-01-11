308650360ec836d89a0492bf15cfe5188aafa1b1
Xabier <xabiergarmendia@ingran.es>
2017-01-11 10:52:54 +0100

    Added functions.

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def send_packet(data,res_size):
     time.sleep(0.01)
 
     ignore=len(to_send)
-    read_val = ser.read(len(tosend)+res_size)
+    read_val = ser.read(len(to_send)+res_size)
 
     read_val_s = read_val[0:ignore]
     if debug > 1:    
@@ -101,6 +101,59 @@ def send_packet(data,res_size):
         print "Data Received: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_r)
     
     return read_val_r
+
+def seed_key(read_val_r):
+    seed = read_val_r[3:5]
+    if debug > 1:
+        print "\tSeed is: %s." % ":".join("{:02x}".format(ord(c)) for c in seed)
+    seed_int=ord(seed[0])*256+ord(seed[1])
+    if debug > 1:
+        print "\tSeed integer: %s." % seed_int
+
+    counter=0
+
+    key=[]
+
+    with open("key.txt") as f:
+        for line in f:
+            if (counter==seed_int):
+                content = int(line,16)
+                if debug > 1:
+                    print "\tKey integer: %s." % content
+                hex_1=int(line[0:2],16)
+                hex_2=int(line[2:4],16)
+                key=chr(hex_1)+chr(hex_2)
+                if debug > 1:
+                    print "\tKey hex: %s." % ":".join("{:02x}".format(ord(c)) for c in key)
+            
+            
+            counter=counter+1
+    
+    key_answer=b"\x04\x27\x02"+key
+    return key_answer
+
+def get_rpm():
+    response=send_packet(b"\x02\x21\x09",6)
+    if len(response)<6:
+        rpm=0
+    else:
+        rpm=ord(response[3])*256+ord(response[4])
+    
+    return rpm
+    
+def get_bvolt():
+    response=send_packet(b"\x02\x21\x10",8)
+    if len(response)<8:
+        b_voltage=0
+    else:
+        b_voltage=ord(response[3])*256+ord(response[4])
+    
+    b_voltage=float(b_voltage)/1000
+    
+    return b_voltage
+    
+    
+    
     
 os.system("cls")
 print ""
@@ -110,140 +163,38 @@ print ""
 print "Initing..."
 fast_init()
 
-ser = serial.Serial(serial_port, 10400, timeout=0.1) #CP210x is configured
+ser = serial.Serial(serial_port, 10400, timeout=0.1)    #CP210x must be configured for 
 
-# Init Frame
-init1=b"\x81\x13\xF7\x81\x0C" 
-ser.write(init1)
-time.sleep(0.01)
+time.sleep(0.1)
+response=send_packet(b"\x81\x13\xF7\x81",5)             #Init Frame
+time.sleep(0.1)
+response=send_packet(b"\x02\x10\xA0",3)             #Start Diagnostics
+time.sleep(0.1)
+response=send_packet(b"\x02\x27\x01",6)             #Seed Request
 
-ignore=len(init1)
-read_val = ser.read(size=15)
+if (len(response)==6):
+    key_ans=seed_key(response)
+    response=send_packet(key_ans,4)             #Seed Request
 
-read_val_s = read_val[0:ignore]
-print "Data Sent: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_s)
-read_val_r = read_val[ignore:]
-print "Data Received: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_r)
-
-#Start Diagnostics
-init1=b"\x02\x10\xA0\xB2"
-ser.write(init1)
-time.sleep(0.01)
-
-ignore=len(init1)
-read_val = ser.read(size=15)
-
-read_val_s = read_val[0:ignore]
-print "Data Sent: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_s)
-read_val_r = read_val[ignore:]
-print "Data Received: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_r)
-
-#Seed Req
-init1=b"\x02\x27\x01\x2A"
-ser.write(init1)
-time.sleep(0.01)
-
-ignore=len(init1)
-read_val = ser.read(size=15)
-
-read_val_s = read_val[0:ignore]
-print "Data Sent: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_s)
-read_val_r = read_val[ignore:]
-print "Data Received: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_r)
-seed = read_val_r[3:5]
-print "\tSeed is: %s." % ":".join("{:02x}".format(ord(c)) for c in seed)
-seed_int=ord(seed[0])*256+ord(seed[1])
-print "\tSeed integer: %s." % seed_int
-
-counter=0
-
-key=[]
-
-with open("key.txt") as f:
-    for line in f:
-        if (counter==seed_int):
-            content = int(line,16)
-            print "\tKey integer: %s." % content
-            hex_1=int(line[0:2],16)
-            hex_2=int(line[2:4],16)
-            key=chr(hex_1)+chr(hex_2)
-            print "\tKey hex: %s." % ":".join("{:02x}".format(ord(c)) for c in key)
-        
-        
-        counter=counter+1
-
-modulo = (int('0x04',16) + int('0x27',16) +int('0x02',16) + ord(key[0]) + ord(key[1])) %256;
-        
-#Key Send
-init1=b"\x04\x27\x02"+key+chr(modulo)
-ser.write(init1)
-time.sleep(0.01)
-
-ignore=len(init1)
-read_val = ser.read(size=15)
-
-read_val_s = read_val[0:ignore]
-print "Data Sent: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_s)
-read_val_r = read_val[ignore:]
-print "Data Received: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_r)
-
-#Start fuelling request
-modulo = (int('0x02',16) + int('0x21',16) +int('0x02',16)) %256;
-    
-init1=b"\x02\x21\x02"+chr(modulo)
-ser.write(init1)
-time.sleep(0.01)
-
-ignore=len(init1)
-read_val = ser.read(size=15)
-
-read_val_s = read_val[0:ignore]
-print "Data Sent: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_s)
-read_val_r = read_val[ignore:]
-print "Data Received: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_r)
+time.sleep(0.1)
+response=send_packet(b"\x02\x21\x02",15)             #Start Diagnostics
 
 time.sleep(2)
 
+debug=0
 
 #Start requesting data
 while (True):
     time.sleep(0.01)
-    modulo = (int('0x02',16) + int('0x21',16) +int('0x10',16)) %256;
+    b_voltage=get_bvolt()
     
-    init1=b"\x02\x21\x10"+chr(modulo)
-    ser.write(init1)
-    time.sleep(0.01)
-
-    ignore=len(init1)
-    read_val = ser.read(size=12)
-
-    read_val_s = read_val[0:ignore]
-    #print "Data Sent: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_s)
-    read_val_r = read_val[ignore:]
-    #print "Data Received: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_r)
+    time.sleep(0.01) 
+    rpm=get_rpm()
     
-    b_voltage=ord(read_val_r[3])*256+ord(read_val_r[4])
-    
-    time.sleep(0.01)
-    modulo = (int('0x02',16) + int('0x21',16) +int('0x09',16)) %256;
-    
-    init1=b"\x02\x21\x09"+chr(modulo)
-    ser.write(init1)
-    time.sleep(0.01)
-
-    ignore=len(init1)
-    read_val = ser.read(size=10)
-
-    read_val_s = read_val[0:ignore]
-    #print "Data Sent: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_s)
-    read_val_r = read_val[ignore:]
-    #print "Data Received: %s." % ":".join("{:02x}".format(ord(c)) for c in read_val_r)
-    
-    rpm=ord(read_val_r[3])*256+ord(read_val_r[4])
     os.system("cls")
     print "\t\t Td5 Storm"
     print " "
-    print "\t Voltaje de bateria: ", str(float(b_voltage)/1000)
+    print "\t Voltaje de bateria: ", str(b_voltage)
     print "\t RPM: ", str(rpm)
 
 
