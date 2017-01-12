176d33d7147d1162e8023f7db2afb15689860c38
Xabier <xabiergarmendia@ingran.es>
2017-01-12 09:45:23 +0100

    Now Seed-Key is calculated using Paul's argorithm

--- main.py
+++ main.py
@@ -106,33 +106,72 @@ def send_packet(data,res_size):
     return read_val_r
 
 def seed_key(read_val_r):
+    # seed = read_val_r[3:5]
+    # if debug > 1:
+        # print "\tSeed is: %s." % ":".join("{:02x}".format(ord(c)) for c in seed)
+    # seed_int=ord(seed[0])*256+ord(seed[1])
+    # if debug > 1:
+        # print "\tSeed integer: %s." % seed_int
+
+    # counter=0
+
+    # key=[]
+
+    # with open("key.txt") as f:
+        # for line in f:
+            # if (counter==seed_int):
+                # content = int(line,16)
+                # if debug > 1:
+                    # print "\tKey integer: %s." % content
+                # hex_1=int(line[0:2],16)
+                # hex_2=int(line[2:4],16)
+                # key=chr(hex_1)+chr(hex_2)
+                # if debug > 1:
+                    # print "\tKey hex: %s." % ":".join("{:02x}".format(ord(c)) for c in key)
+            
+            
+            # counter=counter+1
+    
+
+    # key_answer=b"\x04\x27\x02"+key
+    # return key_answer
+    
     seed = read_val_r[3:5]
     if debug > 1:
         print "\tSeed is: %s." % ":".join("{:02x}".format(ord(c)) for c in seed)
+    
     seed_int=ord(seed[0])*256+ord(seed[1])
     if debug > 1:
         print "\tSeed integer: %s." % seed_int
-
-    counter=0
-
-    key=[]
-
-    with open("key.txt") as f:
-        for line in f:
-            if (counter==seed_int):
-                content = int(line,16)
-                if debug > 1:
-                    print "\tKey integer: %s." % content
-                hex_1=int(line[0:2],16)
-                hex_2=int(line[2:4],16)
-                key=chr(hex_1)+chr(hex_2)
-                if debug > 1:
-                    print "\tKey hex: %s." % ":".join("{:02x}".format(ord(c)) for c in key)
-            
-            
-            counter=counter+1
     
+    seed=seed_int
+
+    count = ((seed >> 0xC & 0x8) + (seed >> 0x5 & 0x4) + (seed >> 0x3 & 0x2) + (seed & 0x1)) + 1
+
+    idx = 0
+    while (idx < count):
+            tap = ((seed >> 1) ^ (seed >> 2 ) ^ (seed >> 8 ) ^ (seed >> 9)) & 1
+            tmp = (seed >> 1) | ( tap << 0xF)
+            if (seed >> 0x3 & 1) and (seed >> 0xD & 1):
+                    seed = tmp & ~1
+            else:
+                    seed = tmp | 1
+
+            idx = idx + 1
+
+    if (seed<256):
+        high=0x00
+        low=seed
+    else:
+        high=seed/256
+        low=seed%256
+
+    key=chr(high)+chr(low)
+    if debug > 1:
+        print "\tKey hex: %s." % ":".join("{:02x}".format(ord(c)) for c in key)
+        
     key_answer=b"\x04\x27\x02"+key
+    
     return key_answer
 
 def get_rpm():
